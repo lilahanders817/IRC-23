b0c857d0c81b168743c3b12631c394ef0e20a88d
lilahanders817 <143014915+lilahanders817@users.noreply.github.com>
2023-11-28 02:10:57 -0800

    Update AutoDrive.java

--- src/main/java/frc/robot/commands/AutoDrive.java
+++ src/main/java/frc/robot/commands/AutoDrive.java
@@ -3,36 +3,63 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Arm;
 
 public class AutoDrive extends CommandBase {
-    Drivetrain drivetrain;
-    double leftSpeed; 
-    double rightSpeed;
-    Timer timer;
-    double time;
+    private final Drivetrain drivetrain;
+    private final Arm arm;
+    private final Timer timer = new Timer();
 
-    public AutoDrive(Drivetrain dt, double lS, double rS, double t) {
+    public AutoDrive(Drivetrain dt, Arm arm) {
         drivetrain = dt;
-        leftSpeed = lS;
-        rightSpeed = rS;
-        timer = new Timer();
-        time = t;
-        super.addRequirements(dt);
+        this.arm = arm;
+        addRequirements(dt, arm);
     }
 
-    public void initialize(){
+    @Override
+    public void initialize() {
         timer.reset();
         timer.start();
     }
 
+    @Override
     public void execute() {
-        drivetrain.drive(leftSpeed, rightSpeed);
+        double elapsedTime = timer.get();
+
+        // Move forward for 5 seconds
+        if (elapsedTime <= 5.0) {
+            drivetrain.drive(0.5, 0.5); // Adjust speed
+        } else if (elapsedTime <= 8.0) {
+            // Turn left for 90 degrees
+            drivetrain.drive(0.5, -0.5); // Adjust speed
+        } else if (elapsedTime <= 11.0) {
+            // Move forward for 3 seconds
+            drivetrain.drive(0.5, 0.5); // Adjust speed 
+        } else if (elapsedTime <= 13.0) {
+            // Turn right for 90 degrees
+            drivetrain.drive(-0.5, 0.5); // Adjust speed
+        } else if (elapsedTime <= 14.5) {
+            // Move forward for 1.5 seconds
+            drivetrain.drive(0.5, 0.5); // Adjust speed
+        } else if (elapsedTime <= 15.0) {
+            // Raise arm
+            arm.moveArmUp(); // Adjust method as needed
+        } else {
+            // Stop drivetrain and arm
+            drivetrain.stop();
+            arm.stop();
+        }
     }
 
+    @Override
     public boolean isFinished() {
-        return timer.get() > time;
+        // Finish after 15 seconds
+        return timer.get() > 15.0;
     }
 
-    public void end(boolean stop) {
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.stop();
+        arm.stop();
     }
 }
