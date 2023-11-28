5f94090c627db8b95400527a236401cb9231db51
lilahanders817 <143014915+lilahanders817@users.noreply.github.com>
2023-11-28 02:12:44 -0800

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,33 +5,32 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.AutoDrive;
-import frc.robot.commands.ResetArmCommand;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
-  Joystick leftJoystick = new Joystick(0);
-  Joystick rightJoystick = new Joystick(1);
-  JoystickButton raiseArm = new JoystickButton(leftJoystick, 2);
-  JoystickButton lowerArm = new JoystickButton(rightJoystick, 2);
-  JoystickButton resetArm = new JoystickButton(leftJoystick, 3); // button for resetting the arm
+    Joystick leftJoystick = new Joystick(0);
+    Joystick rightJoystick = new Joystick(1);
+    JoystickButton raiseArmButton = new JoystickButton(leftJoystick, 2);
+    JoystickButton lowerArmButton = new JoystickButton(rightJoystick, 2);
+    JoystickButton stopArmButton = new JoystickButton(leftJoystick, 3);
 
-  Drivetrain drivetrain = new Drivetrain();
-  Arm arm = new Arm();
+    Drivetrain drivetrain = new Drivetrain();
+    Arm arm = new Arm();
 
-  public RobotContainer() {
-    drivetrain.setDefaultCommand(new InstantCommand(() -> drivetrain.drive(leftJoystick.getY(), rightJoystick.getY()), drivetrain));
-    configureButtonBindings();
-  }
+    public RobotContainer() {
+        drivetrain.setDefaultCommand(new InstantCommand(() -> drivetrain.drive(leftJoystick.getY(), rightJoystick.getY())));
+        configureButtonBindings();
+    }
 
-  private void configureButtonBindings() {
-    raiseArm.onTrue(new InstantCommand(() -> arm.moveArm(0.5)));
-    lowerArm.onTrue(new InstantCommand(() -> arm.moveArm(-0.5)));
-    resetArm.whenPressed(new ResetArmCommand(arm)); // button with the ResetArmCommand
-  }
+    private void configureButtonBindings() {
+    raiseArmButton.whenHeld(new InstantCommand(() -> arm.moveArmUp()));
+    lowerArmButton.whenHeld(new InstantCommand(() -> arm.moveArmDown()));
+    stopArmButton.whenPressed(new InstantCommand(() -> arm.stop()));
+}
 
   public Command getAutonomousCommand() {
-    return new AutoDrive(drivetrain, 0.5, 0.5, 2);
+    return new AutoDrive(drivetrain, arm);
   }
 
   
