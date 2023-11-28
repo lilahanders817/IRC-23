f2d2a65dbeca13a6063503382df6d9280876f739
lilahanders817 <143014915+lilahanders817@users.noreply.github.com>
2023-11-28 02:13:58 -0800

    Update Arm.java

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,21 +1,14 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
     MotorController armVictor;
-    Timer movementTimer;
-    private double zeroPosition = 0.0;
-
-    // Duration for which the arm should move (seconds)
-    private final double movementDuration = 2.0;
 
     public Arm() {
         armVictor = new Victor(2);
-        movementTimer = new Timer();
     }
 
     public void moveArm(double speed) {
@@ -26,26 +19,11 @@ public class Arm extends SubsystemBase {
         armVictor.stopMotor();
     }
 
-    //Move arm for a time
-    public void moveArmForDuration(double speed) {
-        movementTimer.reset();
-        movementTimer.start();
-
-        while (movementTimer.get() < movementDuration) {
-            armVictor.set(speed);
-        }
-
-        stop(); //Stop arm after the time
-        movementTimer.stop();
+    public void moveArmUp() {
+        moveArm(0.2); // Adjust the speed
     }
 
-    //set the zero position to the lowest point of the arm
-    public void setZeroPosition() {
-        zeroPosition = 0.0; // Make value of the lowest point of the arm 
-    }
-
-    //Reset the arm to the zero position
-    public void resetToZero() {
-        moveArm(zeroPosition);
+    public void moveArmDown() {
+        moveArm(-0.2); // Adjust the speed
     }
 }
